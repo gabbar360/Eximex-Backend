72610c97a89ebd5aad816bdee5078f4b03d990d2
Jaydeep Sagalaka <jaydeepsagalaka@gmail.com>
2025-10-16 16:27:52 +0530

    add live origin

--- src/index.js
+++ src/index.js
@@ -44,7 +44,8 @@ const getAllowedOrigins = () => {
   origins.push(
     'http://localhost:5173',
     'http://localhost:3000',
-    'http://localhost:5000'
+    'http://localhost:5000',
+    'https://eximexperts.in'
   );
 
   return [...new Set(origins)]; // Remove duplicates
